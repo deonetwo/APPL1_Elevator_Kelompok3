5594be7d4190f89206f3f7b3de66129245f82f20
mufqiuwais <mufqi.uwais.tif418@polban.ac.id>
2020-04-11 18:11:47 +0700

    init FloorRequest

---
+++ src/controller/FloorRequestLogger.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class FloorRequestLogger {
+    //floorNumber
+    //cabID
+    //queue requestQueue
+
+    //AddFloorRequestToQueue()
+    //FloorRequestButtonPressed(cabID, floorNumber) ??????
+}
