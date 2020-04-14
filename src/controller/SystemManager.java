017b8cdf10add5791a1c7ac958cefdf43b1d1003
khoirunnisapk <48423384+khoirunnisapk@users.noreply.github.com>
2020-04-14 19:37:16 +0700

    Update SystemManager.java

--- src/controller/SystemManager.java
+++ src/controller/SystemManager.java
@@ -1,32 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
-import component.FloorRequest;
-import component.MaintenanceSwitch;
+
 
 /**
  *
  * @author User
  */
 public class SystemManager {
-    private FloorRequest floor;
-    public SystemManager(FloorRequest floor){
-        this.floor=floor;
+   
+    public SystemManager(){
+        
     }
     public void ShutDown(){
         //tutup pintu
-        floor.turnLightOff();
+        
         //matiin lampu summon button 
        //ngestop lift
         
     }
     public void Initialize(){
-        floor.turnLightOn();
-        //nyalain lampu summon button
+      
     }
     
 }
