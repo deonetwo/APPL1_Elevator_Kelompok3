438181a27c2dfff11381b0092613d1a45b5c2070
alfarroqie <48423485+alfarroqie@users.noreply.github.com>
2020-04-14 18:25:52 +0700

    Create SummonRequestLogger.java

---
+++ src/controller/SummonRequestLogger.java
@@ -0,0 +1,17 @@
+package controller;
+
+/**
+ *
+ * @author Chofief dan Fajrina
+ */
+
+import component.SummonButton;
+
+public class SummonRequestLogger {
+    
+    /*
+    public void AddSummonRequestToQueue(){
+    }  
+    */
+    
+}
